7e7e834806fd86561ce7da057eb39fc03c8ee467
small-dogg <sy920112@naver.com>
2023-01-27 00:12:06 +0900

    feat: LinearLayout

---
+++ app/src/main/java/com/smalldogg/study/android/Activity1.kt
@@ -0,0 +1,11 @@
+package com.smalldogg.study.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity1)
+    }
+}
