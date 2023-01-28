5d2df1483b114493f9792f547cae344ad395ca79
small-dogg <sy920112@naver.com>
2023-01-29 01:34:06 +0900

    feat: relativeLayout

---
+++ app/src/main/java/com/smalldogg/study/android/ConstraintLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.smalldogg.study.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.constraint_layout_activity)
+    }
+}
---
+++ app/src/main/java/com/smalldogg/study/android/RelativeLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.smalldogg.study.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.relative_layout_activity)
+    }
+}
