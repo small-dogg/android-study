8db57ed8f86da143bada98f6ee99b32c205fff79
small-dogg <sy920112@naver.com>
2023-01-30 00:01:09 +0900

    feat: space, GridLayout

---
+++ app/src/main/java/com/smalldogg/study/android/SpaceActivity.kt
@@ -0,0 +1,11 @@
+package com.smalldogg.study.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.space_activity)
+    }
+}
