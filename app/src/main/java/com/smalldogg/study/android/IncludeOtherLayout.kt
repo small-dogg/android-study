f41a023f785933165f1c39cc8561a6043253e10e
small-dogg <sy920112@naver.com>
2023-01-31 00:07:30 +0900

    feat: include other layout

---
+++ app/src/main/java/com/smalldogg/study/android/IncludeOtherLayout.kt
@@ -0,0 +1,11 @@
+package com.smalldogg.study.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IncludeOtherLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.include_other_layout_activity)
+    }
+}
