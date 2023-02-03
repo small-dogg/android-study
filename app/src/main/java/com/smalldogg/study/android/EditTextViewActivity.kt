8d5d7a430c5c021be35e23ab8ecb5c0b552d7d71
small-dogg <sy920112@naver.com>
2023-02-04 00:43:18 +0900

    feat: EditText View 학습

---
+++ app/src/main/java/com/smalldogg/study/android/EditTextViewActivity.kt
@@ -0,0 +1,53 @@
+package com.smalldogg.study.android
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.button_view_activity.*
+import kotlinx.android.synthetic.main.edit_text_view_activity.*
+
+class EditTextViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_text_view_activity)
+
+        getString.setOnClickListener {
+            textView2.text = editText1.text
+
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(editText1.windowToken, 0)
+
+            editText1.clearFocus()
+        }
+
+        editText1.addTextChangedListener(listener1)
+
+        editText1.setOnEditorActionListener { v, actionId, event ->
+            textView2.text = "엔터 버튼을 눌렀습니다."
+            textView3.text = editText1.text
+            false
+        }
+    }
+
+    val listener1 = object : TextWatcher{
+        //문자열 변경 전
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            textView2.text = "before : ${s}"
+        }
+
+        //문자열 변경 작업이 완료되었을 때
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            textView3.text = "changed : ${s}"
+        }
+
+        //변경된 문자열 화면에 반영도이ㅓㅆ을 때
+        override fun afterTextChanged(s: Editable?) {
+            textView4.text = "after : ${s}"
+        }
+    }
+
+}
