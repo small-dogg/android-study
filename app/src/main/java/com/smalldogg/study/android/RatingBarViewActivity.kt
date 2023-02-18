8c3e64c73b583b7eb18401874183ceb4ac46f7f0
small-dogg <sy920112@naver.com>
2023-02-18 23:50:54 +0900

    feat: RatingBar 학습

---
+++ app/src/main/java/com/smalldogg/study/android/RatingBarViewActivity.kt
@@ -0,0 +1,17 @@
+package com.smalldogg.study.android
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.button_view_activity.*
+
+class RatingBarViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.rating_bar_view_activity)
+
+    }
+
+
+}
