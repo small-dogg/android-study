4f8369240f475d0dcfce5213f7e44d6b529e9c94
small-dogg <sy920112@naver.com>
2023-02-20 18:46:18 +0900

    feat: Card View 학습

---
+++ app/src/main/java/com/smalldogg/study/android/CardViewActivity.kt
@@ -0,0 +1,12 @@
+package com.smalldogg.study.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CardViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.card_view_activity)
+    }
+
+}
