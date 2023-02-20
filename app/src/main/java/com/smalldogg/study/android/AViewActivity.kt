4631e54002661a95b23f3c1a7f3a2de696a6d3e3
small-dogg <sy920112@naver.com>
2023-02-20 18:44:51 +0900

    chore: test

--- app/src/main/java/com/smalldogg/study/android/AViewActivity.kt
+++ app/src/main/java/com/smalldogg/study/android/AViewActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class AViewActivity : AppCompatActivity() {
+    //commit test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.a_view_activity)
