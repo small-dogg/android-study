89467d50d22989816ee25a056b742ef15221c971
small-dogg <sy920112@naver.com>
2023-03-05 21:14:24 +0900

    feat: 경마게임(2)

--- app/src/main/java/com/smalldogg/study/android/HorseRacingActivity.kt
+++ app/src/main/java/com/smalldogg/study/android/HorseRacingActivity.kt
@@ -5,26 +5,30 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.horse_racing_activity.*
 import java.util.*
+import kotlin.concurrent.timer
 
 class HorseRacingActivity : AppCompatActivity() {
+
+
+    var timer : Timer? = null
+    var deltaTime = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.horse_racing_activity)
         textView43.text=""
-        play.setOnClickListener(listener1)
+
+        play.setOnClickListener { TimeFun() }
 
     }
 
-    val listener1 = object : View.OnClickListener {
-        override fun onClick(v: View?) {
-            while (horse1.progress != 100) {
-                Thread.sleep(100)
-                val run = Random().nextInt(2)
-                textView43.append("$run")
-                horse1.incrementProgressBy(run)
-            }
+    fun TimeFun() {
+        timer = timer(period = 100, initialDelay = 0){
+            val nextInt = Random().nextInt(2)
+            if(deltaTime > 100) cancel()
+            deltaTime += nextInt
+            horse1.setProgress(deltaTime)
         }
-
     }
 
 }
