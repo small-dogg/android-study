6e8066656727aae43b9d1ffc4f097bc51753dc51
small-dogg <sy920112@naver.com>
2023-03-08 23:50:53 +0900

    chore: code clean

--- app/src/main/java/com/smalldogg/study/android/HorseRacingActivity.kt
+++ app/src/main/java/com/smalldogg/study/android/HorseRacingActivity.kt
@@ -1,7 +1,6 @@
 package com.smalldogg.study.android
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.horse_racing_activity.*
 import java.util.*
@@ -10,22 +9,22 @@ import kotlin.concurrent.timer
 class HorseRacingActivity : AppCompatActivity() {
 
 
-    var timer : Timer? = null
+    var timer: Timer? = null
     var deltaTime = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.horse_racing_activity)
-        textView43.text=""
+        textView43.text = ""
 
         play.setOnClickListener { TimeFun() }
 
     }
 
     fun TimeFun() {
-        timer = timer(period = 100, initialDelay = 0){
+        timer = timer(period = 100, initialDelay = 0) {
             val nextInt = Random().nextInt(2)
-            if(deltaTime > 100) cancel()
+            if (deltaTime > 100) cancel()
             deltaTime += nextInt
             horse1.setProgress(deltaTime)
         }
